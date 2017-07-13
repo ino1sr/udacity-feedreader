0b074060f6e71937f41c80e95bc3eb3ff193e221
Meine Goy <meine@goyman.com>
2017-07-13 03:33:21 +0200

    Add feed load tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -4,16 +4,8 @@
  * all of the tests that will be run against your application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
-$(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
-    describe('RSS Feeds', function() {
+var runTests = function() {
+    describe("RSS Feeds", function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,7 +13,7 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it("are defined", function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -31,18 +23,18 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('have valid URLs', function() {
+        it("have valid URLs", function() {
             expect(allFeeds).toBeDefined();
             allFeeds.forEach(function(feed) {
-                expect(feed.url).toBeDefined()
+                expect(feed.url).toBeDefined();
                 /* NOTE: I found some URL matching regex online
                  * but I read the RFC is complicated and even seasoned
                  * programmers are using wrong regex
                  * so I didn't copy the use regex I found
                  * and I am just checking for http/https
                  */
-                expect(feed.url).toMatch(/https?:\/\/.+/)
-            })
+                expect(feed.url).toMatch(/https?:\/\/.+/);
+            });
         });
 
 
@@ -50,43 +42,92 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        it('have valid names', function() {
+        it("have valid names", function() {
             expect(allFeeds).toBeDefined();
             allFeeds.forEach(function(feed) {
-                expect(feed.name).toBeDefined()
-                expect(feed.name).not.toBe("")
-            })
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe("");
+            });
         });
     });
 
+    describe("The menu", function() {
 
-    /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* Test that ensures the menu element is
+         * hidden by default.
          */
+        it("should be initially hidden", function() {
+            expect($("body").hasClass("menu-hidden")).toBeTruthy();
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        var menuIcon = $(".menu-icon-link");
 
-    /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Test that ensures the menu changes
+         * visibility when the menu icon is clicked.
+         */
+        it("should change menu visibility on click", function() {
+            expect($("body").hasClass("menu-hidden")).toBeTruthy();
+            menuIcon.trigger( "click" );
+            expect($("body").hasClass("menu-hidden")).toBeFalsy();
+            menuIcon.trigger( "click" );
+            expect($("body").hasClass("menu-hidden")).toBeTruthy();
+        });
+    });
+
+    describe("Initial Entries", function() {
+        beforeEach(function(done) {
+            spyOn(window, "loadFeed").and.callThrough();
+            loadFeed(0, done);
+        });
+
+        /* Test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        it("should have at least one entry", function(done) {
+            expect(window.loadFeed).toHaveBeenCalled();
+            expect($(".feed .entry").length).toBeGreaterThan(0);
+            done();
+        });
+    });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe("New feed selection", function() {
+        var previousContent = {};
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
-}());
+        /* Serialize the titles for comparison */
+        var getContent = function() {
+            return {
+                pageTitle: $(".header-title").html(),
+                entryTitles: $(".feed .entry h2").map(function (idx, e) {
+                    return $(e).html();
+                }).get()
+            };
+        };
+
+        beforeEach(function(done) {
+            spyOn(window, "loadFeed").and.callThrough();
+
+            var loadAgain = function() {
+                /* Read current value (feed 0) */
+                previousContent = getContent();
+                /* Load again a different feed */
+                loadFeed(1, done);
+            };
+
+            /* Ensure the initial feed is loaded */
+            loadFeed(0, loadAgain);
+        });
+
+        it("should change content on new feed load", function(done) {
+            expect(window.loadFeed.calls.count()).toEqual(2);
+            var newContent = getContent();
+            expect(newContent.pageTitle).not.toEqual(previousContent.pageTitle);
+            expect(newContent.entryTitles).not.toEqual(previousContent.entryTitles);
+            done();
+        });
+    });
+}();
+
+/* Ensure our test are run when google api is loaded */
+google.setOnLoadCallback(runTests);
